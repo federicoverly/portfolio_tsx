ad74fc1f4a0e8801291c62047f24418d8f7ff303
Federico Verly <federicoverly@gmail.com>
2022-11-04 15:54:39 +0100

    videos_reduction

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -20,11 +20,6 @@ export const videos = [
       "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/4._Netflix_Clone.mp4?alt=media&token=9a0c5a26-6a05-4290-b78c-90be368e3a80",
   },
   {
-    title: "Spotify Clone",
-    video:
-      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/5._Spotify_Clone.mp4?alt=media&token=3dba37d3-3539-4849-9b69-1f33ead62556",
-  },
-  {
     title: "Discord Clone",
     video:
       "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/6._Discord_Clone.mp4?alt=media&token=88a545bc-3cde-4467-92e9-2c23b7eb1724",
@@ -35,11 +30,6 @@ export const videos = [
       "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/8._Tinder_Clone.mp4?alt=media&token=0e103ea9-7f54-459f-ab10-04c92c7cd40d",
   },
   {
-    title: "Twitch Clone",
-    video:
-      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/9._Twitch_Clone.mp4?alt=media&token=cee88688-779b-4d68-bfe6-eb2ce1cedb59",
-  },
-  {
     title: "Whatsapp Clone",
     video:
       "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/10._Whatsapp_Clone.mp4?alt=media&token=fc13597e-28f3-4c32-824f-1f360a72dfbf",
@@ -48,16 +38,6 @@ export const videos = [
     title: "Blog",
     video:
       "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/12._Blog.mp4?alt=media&token=54b6706e-dacb-4bb2-b971-4c81ed2a0052",
-  },
-  {
-    title: "Landing Example 1",
-    video:
-      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/11._Restaurant.mp4?alt=media&token=12aaf3a7-cf46-4c9b-bc2e-3e9eaecadb33",
-  },
-  {
-    title: "Landing Example 2",
-    video:
-      "https://firebasestorage.googleapis.com/v0/b/federicoverly-56c58.appspot.com/o/7._Del_Arroyo.mp4?alt=media&token=b2feda22-3b97-44b8-b5b1-2db7fa5ef90a",
   },
 ];
 
